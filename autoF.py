47aade41da1e4ef545e2f97546c4634e9e96c3b0
aleix <obitoyelmaki@hotmail.com>
2022-02-04 19:05:23 +0100

    Update autoF.py

--- autoF.py
+++ autoF.py
@@ -6,24 +6,34 @@ import tkinter
 from win32gui import GetWindowText, GetForegroundWindow
 import time
 
-
+#
 pyautogui.PAUSE = 0
 pyautogui.FAILSAFE = False
 pyautogui.MINIMUM_SLEEP = 0
 pyautogui.MINIMUM_DURATION = 0
 
 pressed_f = False
+BarSpam = False
 windowName = "Genshin Impact"
 
 
-def pressedF(kb_event_info):
+def toggleF(kb_event_info):
     global pressed_f
     pressed_f = not pressed_f
     showText()
+    
+def toggleBarOn(kb_event_info):
+    global BarSpam
+    BarSpam = True
+    
+def toggleBarOff(kb_event_info):
+    global BarSpam
+    BarSpam = False
 
 
 def showText():
     global pressed_f
+    global label
     label = tkinter.Label(text='autoF', font=(None, '12', 'bold'), fg='white', bg='black')
     if ((pressed_f == True) & get_active_window()):
         label.master.overrideredirect(True)
@@ -43,13 +53,18 @@ def get_active_window():
     return (GetWindowText(GetForegroundWindow()) == windowName)
 
 
-
 def main():
     global pressed_f
+    global BarSpam
     if __name__== "__main__" :
-        keyboard.on_press_key('f',pressedF)
+        keyboard.on_press_key('º',toggleF)
+        keyboard.on_press_key('ctrl',toggleBarOn)
+        keyboard.on_release_key('ctrl',toggleBarOff)
         while True:
             while (get_active_window()):
+                if BarSpam:
+                    pyautogui.press("Space")
+                    pyautogui.sleep(randint(10, 400)/1000)
                 if pressed_f:
                     pyautogui.press("f")
                     pyautogui.sleep(randint(10, 400)/1000)
