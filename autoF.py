dbb82a5664052692863048ced8dafdffdc5dc3cb
nengoxx <obitoyelmaki@hotmail.com>
2023-07-08 16:48:36 +0200

    Update autoF.py
    spamclick

--- autoF.py
+++ autoF.py
@@ -15,6 +15,8 @@ pyautogui.MINIMUM_DURATION = 0
 
 pressed_f = False
 BarSpam = False
+LeftSpam = False
+label = None
 windowName = "Genshin Impact"
 
 
@@ -36,19 +38,28 @@ def toggleBarOff(kb_event_info):
     global BarSpam
     BarSpam = False
 
+def toggleLeftClick(kb_event_info):
+    global LeftSpam
+    LeftSpam = not LeftSpam
+    showText('autoC')
+
 def on_click(x, y, button, pressed):
     if pressed:
-        if (button == mouse.Button.x1) or (button == mouse.Button.x1):
+        if (button == mouse.Button.x1):
             toggleF(None)
+            return
+        elif (button == mouse.Button.x2):
+            toggleLeftClick(None)
             return
         else:
             return
 
-def showText():
+def showText(itext = 'autoF'):
     global pressed_f
+    global LeftSpam
     global label
-    label = tkinter.Label(text='autoF', font=(None, '12', 'bold'), fg='white', bg='black')
-    if ((pressed_f == True) & get_active_window()):
+    label = tkinter.Label(text=itext, font=(None, '12', 'bold'), fg='white', bg='black')
+    if (get_active_window() & (pressed_f == True or LeftSpam == True)):
         label.master.overrideredirect(True)
         #label.master.geometry("+5+5")
         label.master.lift()
@@ -91,6 +102,11 @@ def main():
                 if BarSpam:
                     pyautogui.press("Space")
                     pyautogui.sleep(randint(300, 600)/1000)
+                    continue
+                if LeftSpam:
+                    pyautogui.click()
+                    pyautogui.sleep(randint(300, 600)/1000)
+                    continue
                 if pressed_f:
                     pyautogui.press("f")
                 pyautogui.sleep(randint(150, 250)/1000)
